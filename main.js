c29ef5438975a136b9d66782e239aa3963adcf0f
bob <sammy.chesire@student.moringaschool.com>
2022-08-27 10:52:41 +0300

    completed assignment

--- main.js
+++ main.js
@@ -3,6 +3,26 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errorModal = document.querySelector("#modal");
+errorModal.classList.add("hidden");
+
+const hearts = document.querySelectorAll(".like-glyph");
+hearts.forEach((heart) => {
+  heart.addEventListener("click", () => {
+    mimicServerCall()
+      .then((res) => {
+        heart.innerHTML = heart.innerHTML === FULL_HEART ? EMPTY_HEART : FULL_HEART;
+        heart.classList.toggle("activated-heart");
+      })
+      .catch((error) => {
+        errorModal.classList.remove("hidden");
+        errorModal.children[1].textContent = error;
+        setTimeout(() => {
+          errorModal.classList.add("hidden");
+        }, 3000);
+      });
+  });
+});
 
 
 
